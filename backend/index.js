c45e247c629f55d2fdec7ef0812980340f3d485e
pramirezd <pablo.ramirez.fcfm@gmail.com>
2024-06-29 22:57:54 -0400

    Opcion PORT index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 app.use("/", api);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
